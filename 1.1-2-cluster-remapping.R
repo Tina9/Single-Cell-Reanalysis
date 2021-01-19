aa3d33251c10022cf7015a6a21d5891317331654
xu zhang <xuhzhang@outlook.com>
2021-01-19 18:02:27 +0800

    add the code for annotating clusters using combined data

--- 1.1-2-cluster-remapping.R
+++ 1.1-2-cluster-remapping.R
@@ -78,3 +78,30 @@ label_remapping(HP4568Re, HP4568Re_new_cluster_ids, "HP4568Re")
 HP4568Re <- cluster_metadata(HP4568Re, HP4568Re_new_cluster_ids)
 save(HP4568Re, file = "HP4568Re_no_doublets.RData")
 
+##########################################################################
+##########################################################################
+########## Annotating cluster using combined data ########################
+##########################################################################
+##########################################################################
+load("~/Dropbox/BRCA1-PARPi-10X/Latest-with-doublets-Version/HP4313/HP4313_combined_data.RData")
+load("~/Dropbox/BRCA1-PARPi-10X/Latest-with-doublets-Version/HP4540/HP4540_combined_data.RData")
+load("~/Dropbox/BRCA1-PARPi-10X/Latest-with-doublets-Version/HP4568/HP4568_combined_data.RData")
+HP4540Com_new_cluster_ids <- c("Tumor", "Fibroblast", "Tumor", "Macrophages", "Tumor",
+                              "T cells", "Tumor", "Tumor_proliferating", "Tumor",
+                              "Endothelial cells", "Tumor", "Stromal cells", "Stromal cells",
+                              "Tumor", "Macrophages", "Tumor", "B cells", "T cells",
+                              "Neutrophils/MDSCs", "Undefined", "Tumor", "Fibroblast")
+label_remapping(HP4540_combined_data, HP4540Com_new_cluster_ids, "HP4540Com")
+
+HP4313Com_new_cluster_ids <- c("Tumor", "Tumor", "Macrophages", "Tumor_proliferating",
+                               "Tumor", "Neutrophils/MDSCs", "Tumor", "Tumor",
+                               "T cells", "Macrophages", "Macrophages", "Fibroblasts",
+                               "Endothelial cells", "Fibroblasts", "Undefined", "B cells")
+label_remapping(HP4313_combined_data, HP4313Com_new_cluster_ids, "HP4313Com")
+
+HP4568Com_new_cluster_ids <- c("Tumor", "Tumor", "Macrophages", "Tumor_proliferating",
+                               "Tumor", "Neutrophils/MDSCs", "T cells", "Tumor",
+                               "Tumor", "B cells", "T cells", "Endothelial cells",
+                               "Fibroblasts", "Fibroblasts", "Tumor", "Macrophages",
+                               "Tumor", "Tumor")
+label_remapping(HP4568_combined_data, HP4568Com_new_cluster_ids, "HP4568Com")
