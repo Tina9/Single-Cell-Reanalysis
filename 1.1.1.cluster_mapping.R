5cb8c9f4c6a0d88f428f1bb70687717f7adeee80
xu zhang <xuhzhang@outlook.com>
2021-01-20 17:10:03 +0800

    fix the bug of removing row name for meta data

--- 1.1.1.cluster_mapping.R
+++ 1.1.1.cluster_mapping.R
@@ -1,11 +1,20 @@
 ####### Integrating Cluster Information into Metadata #################
 cluster_metadata <- function(pbmc, identified_clusters){
   seurat_clusters <- levels(pbmc@meta.data$seurat_clusters)
-  cluster_refer <- cbind(seurat_clusters, identified_clusters)
-  cluster_meta <- merge(pbmc@meta.data, cluster_refer, 
-                        by.x = "seurat_clusters",
-                        by.y = "seurat_clusters",
-                        all.x = T)
-  pbmc@meta.data <- cluster_meta
+  cluster_refer <- as.data.frame(cbind(seurat_clusters, identified_clusters))
+  meta_data <- pbmc@meta.data
+  meta_data$identified_clusters <- cluster_refer[match(meta_data$seurat_clusters, cluster_refer$seurat_clusters), "identified_clusters"]
+  pbmc@meta.data <- meta_data
   return(pbmc)
 }
+
+
+# load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4540Pr/HP4540Pr_no_doublets.RData")
+# pbmc <- HP4540Pr
+# identified_clusters <- c("Tumor", "Fibroblasts", "Macrophage", "T cells",
+#                          "Tumor", "Tumor", "Fibroblasts", "Fibroblasts",
+#                          "Tumor", "Tumor", "Endothelia", "Undefined",
+#                          "B cells", "Fibroblasts", "Fibroblasts", "MDSCs",
+#                          "Macrophage")
+# pbmc <- cluster_metadata(pbmc, identified_clusters)
+
