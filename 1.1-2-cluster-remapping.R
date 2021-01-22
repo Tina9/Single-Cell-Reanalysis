3330c863baae145a265caed4131248d3370af6e3
xu zhang <xuhzhang@outlook.com>
2021-01-22 17:44:05 +0800

    Changed the name for clusters

--- 1.1-2-cluster-remapping.R
+++ 1.1-2-cluster-remapping.R
@@ -17,9 +17,9 @@ label_remapping <- function(pbmc, new_cluster_ids, sample_name){
 setwd("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4540Pr/")
 load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4540Pr/HP4540Pr_no_doublets.RData")
 HP4540Pr_new_cluster_ids <- c("Tumor", "Fibroblasts", "Macrophage", "T cells",
-                              "Tumor", "Tumor-proliferating", "Fibroblasts-proliferating", "Fibroblasts",
+                              "Tumor", "Tumor", "Fibroblasts", "Fibroblasts",
                               "Tumor", "Tumor", "Endothelia", "Undefined",
-                              "B cells", "Fibroblasts", "Fibroblasts", "Neutrophils",
+                              "B cells", "Fibroblasts", "Fibroblasts", "MDSCs",
                               "Macrophage")
 label_remapping(HP4540Pr, HP4540Pr_new_cluster_ids, "HP4540Pr")
 HP4540Pr <- cluster_metadata(HP4540Pr, HP4540Pr_new_cluster_ids)
@@ -29,8 +29,8 @@ setwd("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4540Re/")
 load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4540Re/HP4540Re2_no_doublets.RData")
 HP4540Re_new_cluster_ids <- c("Tumor", "Tumor", "Tumor", "Macrophage",
                               "Tumor", "T cells", "Tumor", "Tumor",
-                              "Tumor-proliferating", "Endothelia", "Fibroblasts",
-                              "Tumor", "stromal", "T cells/NKT", "Epithelia",
+                              "Tumor", "Endothelia", "Fibroblasts",
+                              "Tumor", "stromal", "T cells", "Epithelia",
                               "Neutrophils", "stromal")
 label_remapping(HP4540Re2, HP4540Re_new_cluster_ids, "HP4540Re2")
 HP4540Re2 <- cluster_metadata(HP4540Re2, HP4540Re_new_cluster_ids)
@@ -38,7 +38,7 @@ save(HP4540Re2, file = "HP4540Re2_no_doublets.RData")
 
 setwd("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4313Pr/")
 load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4313Pr/HP4313Pr_no_doublets.RData")
-HP4313Pr_new_cluster_ids <- c("Tumor", "Tumor-proliferating", "Tumor", "Macrophage",
+HP4313Pr_new_cluster_ids <- c("Tumor", "Tumor", "Tumor", "Macrophage",
                               "Tumor", "Tumor", "MDSCs", "T cells",
                               "Macrophage", "T cells", "Fibroblasts", "Fibroblasts",
                               "stromal", "Endothelia", "DC")
@@ -49,7 +49,7 @@ save(HP4313Pr, file = "HP4313Pr_no_doublets.RData")
 setwd("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4313Re/")
 load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4313Re/HP4313Re_no_doublets.RData")
 HP4313Re_new_cluster_ids <- c("Tumor", "Tumor", "Macrophage", "Tumor",
-                              "Tumor-proliferating", "Tumor", "MDSCs",
+                              "Tumor", "Tumor", "MDSCs",
                               "T cells", "Macrophage", "Fibroblasts", "Undefined",
                               "Endothelia")
 label_remapping(HP4313Re, HP4313Re_new_cluster_ids, "HP4313Re")
@@ -59,21 +59,21 @@ save(HP4313Re, file = "HP4313Re_no_doublets.RData")
 
 setwd("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4568Pr/")
 load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4568Pr/HP4568Pr_no_doublets.RData")
-HP4568Pr_new_cluster_ids <- c("Undefined", "Tumor", "Tumor", "Tumor",
-                              "Tumor", "Endothelia", "Macrophage",
-                              "Tumor", "Undefined", "Tumor", "Undefined",
-                              "Tumor", "Fibroblasts", "T cells", "MDSC/Neutrophils",
-                              "Endothelia")
+HP4568Pr_new_cluster_ids <- c("Undefined", "Tumor", "Endothelia", "Macrophage",
+                              "Macrophage", "Undefined", "MDSCs", "Macrophage",
+                              "Macrophage", "Tumor", "Macrophage", "Macrophage",
+                              "Tumor", "Tumor", "Fibroblasts", "T cells",
+                              "Tumor", "MDSCs", "Endothelia")
 label_remapping(HP4568Pr, HP4568Pr_new_cluster_ids, "HP4568Pr")
 HP4568Pr <- cluster_metadata(HP4568Pr, HP4568Pr_new_cluster_ids)
 save(HP4568Pr, file = "HP4568Pr_no_doublets.RData")
 
 setwd("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4568Re/")
 load("~/Dropbox/BRCA1-PARPi-10X/Latest-Version/HP4568Re/HP4568Re_no_doublets.RData")
-HP4568Re_new_cluster_ids <- c("Tumor", "Macrophage", "Tumor", "Tumor", "Tumor-proliferating",
-                              "T cells", "Neutrophils", "Tumor", "B cells", "T cells",
-                              "Endothelia", "Macrophage", "Fibroblasts", "Macrophage-proliferating",
-                              "Fibroblasts")
+HP4568Re_new_cluster_ids <- c("Tumor", "Macrophage", "Tumor", "Tumor", 
+                              "Tumor", "T cells", "MDSCs", "Tumor", 
+                              "B cells", "T cells", "Endothelia", "Macrophage", 
+                              "Fibroblasts", "Macrophage", "Fibroblasts")
 label_remapping(HP4568Re, HP4568Re_new_cluster_ids, "HP4568Re")
 HP4568Re <- cluster_metadata(HP4568Re, HP4568Re_new_cluster_ids)
 save(HP4568Re, file = "HP4568Re_no_doublets.RData")
